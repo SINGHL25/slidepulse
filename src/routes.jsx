de242e62420ce10adf7fb78bd5a2b0cab7721d65
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-23 00:14:50 +1000

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,1 +1,21 @@
+**`routes.jsx`**
+```jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Host from "./pages/Host";
+import Join from "./pages/Join";
+import Poll from "./pages/Poll";
 
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/host/:sessionId" element={<Host />} />
+        <Route path="/join/:sessionId" element={<Join />} />
+        <Route path="/poll/:sessionId" element={<Poll />} />
+      </Routes>
+    </Router>
+  );
+}
+```
