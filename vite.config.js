adaff304f8d2cf2349dbbdf651a265cb973bfc64
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-23 00:01:40 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-
+// *** vite.config.js (skip if using CRA) ***
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+export default defineConfig({
+  plugins: [react()],
+});
