8e44910f8dfe430cd04d969ea1e79c5cf8a986f2
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-23 00:10:47 +1000

    Update QRCodeJoin.jsx

--- src/ components/QRCodeJoin.jsx
+++ src/ components/QRCodeJoin.jsx
@@ -1,1 +1,14 @@
 
+/ *** src/components/QRCodeJoin.jsx ***
+import React from 'react';
+import QRCode from 'qrcode.react';
+
+export default function QRCodeJoin({ roomId }) {
+  const joinUrl = `${window.location.origin}/room/${roomId}`;
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <QRCode value={joinUrl} size={128} />
+      <p className="text-sm break-all">{joinUrl}</p>
+    </div>
+  );
+}
