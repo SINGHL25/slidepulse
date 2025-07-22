3bf985fef58d41b1329b62cf63bc84b540aea932
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-23 00:03:34 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,19 @@
+// ======================= SRC FILES =========================
+
+// *** src/firebase.js ***
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInAnonymously } from 'firebase/auth';
+import { getDatabase, ref, push, onValue, update } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
 
