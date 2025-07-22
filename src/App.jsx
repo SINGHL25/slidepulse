96073d48544e366885d637ac440446c8476406a2
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-23 00:09:05 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+// *** src/App.jsx ***
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AudiencePage from './pages/AudiencePage';
+import AdminDashboard from './pages/AdminDashboard';
+import { anonymousSignIn } from './firebase';
 
+export default function App() {
+  useEffect(() => {
+    anonymousSignIn();
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomId" element={<AudiencePage />} />
+      <Route path="/admin/:roomId" element={<AdminDashboard />} />
+    </Routes>
+  );
+}
